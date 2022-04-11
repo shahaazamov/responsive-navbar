243569ada2a50b9414349fc492fe999f804f457a
shahaazamov <shokhzhakhon.azamov@bk.ru>
2022-04-11 13:45:40 +0300

    beginning of responsive navbar

---
+++ app.js
@@ -0,0 +1,7 @@
+const mainBtn = document.querySelector('.main-btn')
+const body = document.querySelector('body')
+
+mainBtn.addEventListener('click' , () => {
+    body.classList.add("bg-color")
+    // console.log(1)
+})
